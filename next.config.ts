899f28a7ca0a5ec75876c1e2abc941c4818cedfe
quang.le <quang.le@concentrix.com>
2026-04-14 14:39:13 +0700

    fix: Sitecore embed Iframe

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        // Cho phép chính nó và các domain của Sitecore nhúng Iframe
+                        value: "frame-ancestors 'self' https://*.sitecorecloud.io https://*.sitecore.com https://*.sitecore.net;",
+                    },
+                    {
+                        // Bổ sung thêm header này để đảm bảo các trình duyệt cũ không chặn
+                        key: 'X-Frame-Options',
+                        value: 'ALLOW-FROM https://*.sitecorecloud.io',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
